0bf13e181e1c78326b0ff526fd7e59ac85facb3e
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-01-07 12:29:35 -0300

    :construction: finished globalStyle

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,39 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :focus {
-    outline: 0;
+    outline: 0; 
+    box-shadow: 0 0 0 2px ${(props) => props.theme.yellow};
   }
-  
+
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    line-height: '130%';
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Baloo 2', cursive;
+    line-height: '130%';
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  h3 { 
+    font-size: 1.25rem;
+  }
+
+  h4 {
+    font-size: 1.125rem;
+  }
 `
